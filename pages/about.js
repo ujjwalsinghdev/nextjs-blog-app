e51eb94e058949f7ae1ea9f754ca18574099186e
Ujjwal Singh <ujjwalsingh@MBP.local>
2021-06-16 16:23:58 +0530

    pages & routing

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>about</h1>
+      <p>Software Professional | UI Developer focused in React ecosystem | Leadership | Startups</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ pages/blog/add.js
@@ -0,0 +1,9 @@
+const AddBlogsPage = () => {
+    return (
+        <div>
+            <h1>add events</h1>
+        </div>
+    )
+}
+
+export default AddBlogsPage
